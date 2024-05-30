05c38bee9290c9cd45d30a01655bf2fc57bb4f43
Elias Gomez Fuentes <egomez@informat.cl>
2024-05-30 08:50:01 -0400

    cambio config

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import { useState, useEffect } from 'react';
 import { fetchAuthSession } from 'aws-amplify/auth';
 import { signIn, signOut } from 'aws-amplify/auth';
-import { Amplify } from "aws-amplify";
 import { useRouter } from 'next/navigation';
 import Input from '../common/Input';
 import Cookies from 'js-cookie';
@@ -11,70 +10,11 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
-import { getParams } from '@/api/params.api';
-import { datos_prueba } from './layout';
 
 export default function Home() {
   const router = useRouter();
   const [user, setUser] = useState({ username: '', password: '' });
   
-  
-
-  useEffect(() => {
-    getParameters();
-  }, []);
-
-  const getParameters = async () => {
-    let config;
-    try {
-      if(datos_prueba){
-         config = {
-          "aws_project_region": "us-east-1",
-          "aws_cognito_identity_pool_id": 'us-east-1:f1f25452-aa26-4dec-92d6-d28071f7d052',
-          "aws_cognito_region": "us-east-1",
-          "aws_user_pools_id": 'us-east-1_33vySxqM9',
-          "aws_user_pools_web_client_id": '2nklsjq8odvmumr52fo3d81fiv',
-          "oauth": {},
-          "aws_cognito_username_attributes": [],
-          "aws_cognito_social_providers": [],
-          "aws_cognito_signup_attributes": ["EMAIL"],
-          "aws_cognito_mfa_configuration": "OFF",
-          "aws_cognito_mfa_types": ["SMS"],
-          "aws_cognito_password_protection_settings": {
-            "passwordPolicyMinLength": 8,
-            "passwordPolicyCharacters": []
-          },
-          "aws_cognito_verification_mechanisms": ["EMAIL"]
-        }
-
-      }else{
-        const p = await getParams();
-        config = {
-          "aws_project_region": "us-east-1",
-          "aws_cognito_identity_pool_id": p.data.identity_pool_id,
-          "aws_cognito_region": "us-east-1",
-          "aws_user_pools_id": p.data.user_pools_id,
-          "aws_user_pools_web_client_id": p.data.web_client_id,
-          "oauth": {},
-          "aws_cognito_username_attributes": [],
-          "aws_cognito_social_providers": [],
-          "aws_cognito_signup_attributes": ["EMAIL"],
-          "aws_cognito_mfa_configuration": "OFF",
-          "aws_cognito_mfa_types": ["SMS"],
-          "aws_cognito_password_protection_settings": {
-            "passwordPolicyMinLength": 8,
-            "passwordPolicyCharacters": []
-          },
-          "aws_cognito_verification_mechanisms": ["EMAIL"]
-
-        }
-      
-      }
-      Amplify.configure(config);
-    } catch (error) {
-      console.log('error getting params: ', error);
-    }
-  };
 
   const handleInputChange = (event, keyName) => {
     event.persist();
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,58 +1,13 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { signUp } from "aws-amplify/auth";
 import { useRouter } from 'next/navigation';
-import { Amplify } from "aws-amplify";
 import Link from 'next/link';
 import Input from '../../common/Input';
-import { getParams } from '@/api/params.api';
-// Amplify.configure(config);
 
 export default function Register() {
   const navigate = useRouter();
   const [user, setUser] = useState({ username: "", password: "", email: "", db_name: "", db_host:"", db_port:"" });
-
-  useEffect(() => {
-    getParameters();
-  }, []);
-
-  const getParameters = async () => {
-    try {
-      // const p = await getParams();
-      const config = {
-        "aws_project_region": "us-east-1",
-        // "aws_cognito_identity_pool_id": p.data.identity_pool_id,
-        // "aws_cognito_region": "us-east-1",
-        // "aws_user_pools_id": p.data.user_pools_id,
-        // "aws_user_pools_web_client_id": p.data.web_client_id,
-        "aws_cognito_identity_pool_id": 'us-east-1:f1f25452-aa26-4dec-92d6-d28071f7d052',
-        "aws_cognito_region": "us-east-1",
-        "aws_user_pools_id": 'us-east-1_33vySxqM9',
-        "aws_user_pools_web_client_id": '2nklsjq8odvmumr52fo3d81fiv',
-        "oauth": {},
-        "aws_cognito_username_attributes": [],
-        "aws_cognito_social_providers": [],
-        "aws_cognito_signup_attributes": [
-          "EMAIL"
-        ],
-        "aws_cognito_mfa_configuration": "OFF",
-        "aws_cognito_mfa_types": [
-          "SMS"
-        ],
-        "aws_cognito_password_protection_settings": {
-          "passwordPolicyMinLength": 8,
-          "passwordPolicyCharacters": []
-        },
-        "aws_cognito_verification_mechanisms": [
-          "EMAIL"
-        ]
-      }
-      Amplify.configure(config);
-    } catch (error) {
-      console.log('error getting params: ', error);
-    }
-  };
-
 
   const handleInputChange = (event, keyName) => {
     event.persist();

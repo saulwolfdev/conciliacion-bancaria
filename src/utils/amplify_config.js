db04c8156e5af191d5d4b89beea382992d29da98
soraya_aguilar <saguilar@informat.cl>
2024-06-03 13:40:37 -0400

    ajustes cognito

--- src/utils/amplify_config.js
+++ src/utils/amplify_config.js
@@ -1,5 +1,6 @@
 import { getParams } from "@/api/params.api";
 import { datos_prueba } from "@/app/layout";
+import Cookies from 'js-cookie';
 
 export const getAmplifyConfig = async () => {
     let config;
@@ -24,13 +25,27 @@ export const getAmplifyConfig = async () => {
       }
   
     }else{
-      const p = await getParams(); 
+      let identity_pool_id = Cookies.get('identity_pool_id');
+      let params;
+      if(identity_pool_id){
+        params = {
+          'identity_pool_id': identity_pool_id,
+          'user_pools_id': Cookies.get('user_pools_id'),
+          'web_client_id': Cookies.get('web_client_id'),
+        }
+      }else{
+        const p = await getParams(); 
+        params = p.data
+        Cookies.set('identity_pool_id', p.data.identity_pool_id);
+        Cookies.set('user_pools_id', p.data.user_pools_id);
+        Cookies.set('web_client_id', p.data.web_client_id);
+      }
       config = {
         "aws_project_region": "us-east-1",
-        "aws_cognito_identity_pool_id": p.data.identity_pool_id,
+        "aws_cognito_identity_pool_id": params.identity_pool_id,
         "aws_cognito_region": "us-east-1",
-        "aws_user_pools_id": p.data.user_pools_id,
-        "aws_user_pools_web_client_id": p.data.web_client_id,
+        "aws_user_pools_id": params.user_pools_id,
+        "aws_user_pools_web_client_id": params.web_client_id,
         "oauth": {},
         "aws_cognito_username_attributes": [],
         "aws_cognito_social_providers": [],

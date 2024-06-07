6ba5e0988d1a0bbd2b6e02e59177f1ece45c7c70
Elias Gomez Fuentes <egomez@informat.cl>
2024-06-06 20:35:11 -0400

    maquillaje

--- src/app/dashboard/ordenes/page.js
+++ src/app/dashboard/ordenes/page.js
@@ -133,6 +133,7 @@ useEffect(() => {
 }, []);
 
 const loadProduct = async (numero, anio, mes, sucursal) => {
+  setLoading(true);
   try {
     if(datos_prueba){
       setDataOcR(res);
@@ -145,8 +146,10 @@ const loadProduct = async (numero, anio, mes, sucursal) => {
     }
     setComentario(usuario.AdqOAGl3)
     setOpen(true)
+    setLoading(false);
   } catch (error) {
     console.log('error getting oc: ', error);
+    setLoading(false);
   }
 };
 
@@ -192,7 +195,7 @@ const handleApproval = async (estado, message) => {
 };
 
 const LoadingSpinner = () => (
-  <div className="loading active fixed inset-0 flex items-center justify-center bg-white bg-opacity-50 z-100">
+  <div className="loading active fixed inset-0 flex items-center justify-center bg-white bg-opacity-50 z-1000">
     <div className="absolute animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-customGreen"></div>
     <img src="/images/image.png" className="absolute rounded-full h-24 w-24 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></img>
   </div>

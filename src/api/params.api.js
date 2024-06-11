c2abdee05781fdbf93044dd7ba20cdf8679a21e7
soraya_aguilar <saguilar@informat.cl>
2024-06-11 15:40:54 -0400

    azure

--- src/api/params.api.js
+++ src/api/params.api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 const paramsApi = axios.create({
     // baseURL: 'http://localhost:8000/users/',
-    baseURL: 'https://container-inetevo.r1na2lodul27u.us-east-1.cs.amazonlightsail.com/users/',
+    // baseURL: 'https://container-inetevo.r1na2lodul27u.us-east-1.cs.amazonlightsail.com/users/',
+    baseURL: 'https://inetevo-main.azurewebsites.net/users/',
     headers: {
         'Tenant': typeof window !== 'undefined' ? window.location.hostname.split(".")[0] : ''
     }

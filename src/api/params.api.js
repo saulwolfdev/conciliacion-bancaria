ca8b9d849be3d125923ddd932b99eea78503efdb
Elias Gomez Fuentes <egomez@informat.cl>
2024-06-13 09:45:39 -0400

    release

--- src/api/params.api.js
+++ src/api/params.api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const paramsApi = axios.create({
     // baseURL: 'http://localhost:8000/users/',
     // baseURL: 'https://inetevo-main.azurewebsites.net/users/',
-    baseURL: 'https://inetevo-release.azurewebsites.net/oc/',
+    baseURL: 'https://inetevo-release.azurewebsites.net/users/',
     headers: {
         'Tenant': typeof window !== 'undefined' ? window.location.hostname.split(".")[0] : 'undefined'
     }

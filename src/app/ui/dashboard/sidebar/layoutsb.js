1edda439d0f243729db0df60db974c093b67337e
Elias Gomez Fuentes <egomez@informat.cl>
2024-06-21 09:41:19 -0400

    ajuste codigo lateral

--- src/app/ui/dashboard/sidebar/layoutsb.js
+++ src/app/ui/dashboard/sidebar/layoutsb.js
@@ -1,20 +1,12 @@
 "use client"
 import { Fragment, useState, useEffect, Suspense } from 'react';
 import { Dialog, Menu, Transition, Disclosure } from '@headlessui/react';
-import { ChevronRightIcon, ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import { ChevronRightIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
 import {
   Bars3Icon,
-  BellIcon,
-  Cog6ToothIcon,
-  DocumentDuplicateIcon,
-  HomeIcon,
-  UsersIcon,
   XMarkIcon,
-  CalendarIcon,
-  ChartPieIcon,
   FolderIcon,
 } from '@heroicons/react/24/outline';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import Loading from '@/app/dashboard/loading';
 import { signOut } from 'aws-amplify/auth';
@@ -27,22 +19,6 @@ const userNavigation = [
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
-
-const app_config = [
-  {"nombre":"Aprobaciones / OC","categoria":"Adquisiciones","descripcion":"Sistema de aprobaciones de ordenes de compra","href":"dashboard/ordenes","imagesrc":"/images/aprob.png","status":"Ver Producto","habilitado":false},
-  {"nombre":"Ipropertyss","categoria":"Gestión de Activos","descripcion":"Solución para la gestión de inmuebles strip centers y malls.","href":"http://demoarrendador.iproperty.cl/login/?next=/","imagesrc":"/images/IProperty2.png","status":"No habilitada","habilitado":true},
-  {"nombre":"Portal de RRHH","descripcion":"Solución de autoconsulta de RRHH para empleados.","categoria":"RRHH","href":"http://verticales.inetcloud.cl:8081/rrhh/login.aspx","imagesrc":"/images/PortalRRHH.png","status":"Ver Producto","habilitado":false},
-  {"nombre":"Match Financiero","descripcion":"Solución para la automatización de la conciliación bancaria.","categoria":"Finanzas y Contabilidad","href":"#","imagesrc":"/images/MatchFinanciero.png","status":"Próximamente"},
-  {"nombre":"IFacture V2","descripcion":"Solución de Facturación Electrónica.","categoria":"Ventas y Compras","href":"#","imagesrc":"/images/IFactureV2.png","status":"Próximamente"},
-  {"nombre":"Dashboad Finanzas","descripcion":"Solución para ver la información de cuentas por cobrar y pagar.","categoria":"Analítica","href":"#","imagesrc":"/images/finanzas.png","status":"Próximamente"},
-  {"nombre":"Datawalt","descripcion":"Integración para dashboard financiero contable con IA.","categoria":"Integraciones","href":"#","imagesrc":"/images/Datawalt.png","status":"Ver Producto"},
-  {"nombre":"ControlDoc","descripcion":"Integración con solución de firma electrónica.","categoria":"Integraciones","href":"#","imagesrc":"/images/ControlDoc.png","status":"Ver Producto"},
-  {"nombre":"Rindegastos","descripcion":"Integración que automatiza la rendición de gastos.","categoria":"Integraciones","href":"#","imagesrc":"/images/Rindegastos.png","status":"Ver Producto"},
-  {"nombre":"GetPoint","descripcion":"Integración con plataforma WMS para la gestión de bodegas y logística.","categoria":"Integraciones","href":"#","imagesrc":"/images/Getpoint.png","status":"Ver Producto"},
-  {"nombre":"Multivende","descripcion":"Integración para la gestión de Marketplace y tiendas online.","categoria":"Integraciones","href":"#","imagesrc":"/images/Multivende.png","status":"Próximamente"},
-  {"nombre":"Duemint","descripcion":"Integración para el proceso de gestión de cobranza.","categoria":"Integraciones","href":"#","imagesrc":"/images/Duemint.png","status":"Próximamente"},
-  {"nombre":"SIRC","descripcion":"Integración para descarga del libro de asistencia de Geovictoria o Bnovus.","categoria":"Integraciones","href":"#","imagesrc":"/images/SIRC.png","status":"Ver Producto"}
-];
 
 export default function HomeLayout({ children }) {
 
@@ -60,7 +36,6 @@ export default function HomeLayout({ children }) {
       const config = await getAmplifyConfig();
       try {
         const configApp = JSON.parse(localStorage.getItem('apps_config')) || [];
-        console.log(configApp)
         setApps(configApp);  
       } catch (error) {
         console.error('User not logged in:', error);

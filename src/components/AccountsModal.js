c397ee1777e57e0902cb2821bf9eea82967c064c
Saul Alejos Garay <saulalejos1984@gmail.com>
2024-08-14 14:57:36 -0300

    se modulariza el postBankData

--- src/components/AccountsModal.js
+++ src/components/AccountsModal.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { cuentasContables } from "@/api/fintoc.mock";
 import CustomSearchSelect from "@/components/CustomSearchSelect";
+import { postBankData } from "@/api/postBankData"; 
 
 const AccountsModal = ({ isOpen, onClose, data, onLoad }) => {
   const [selectedAccounts, setSelectedAccounts] = useState([]);
@@ -137,6 +138,7 @@ const AccountsModal = ({ isOpen, onClose, data, onLoad }) => {
 
   const handleLoadClick = () => {
     onLoad(selectedAccounts);
+    postBankData(bankAccountsData);
     onClose();
   };
 
@@ -176,72 +178,36 @@ const AccountsModal = ({ isOpen, onClose, data, onLoad }) => {
       console.log("Updated selectedOption:", newSelectedOption);
       return newSelectedOption;
     });
-  };
-  
-  
+  }; 
   
   console.log("bankAccountsData para enviar:", bankAccountsData)
-
-  const handleSubmit = async () => {
-    const payload = {
-      tipo: 2,
-      banco_id: "link_oObKGalip9eXP8y5",
-      cuentas_bancarias: bankAccountsData.map(account => ({
-        id: account.id,
-        cuenta_contable: account.cuenta_contable,
-        linea_credito: account.id, 
-      })),
-    };
-
-    console.log("Payload completo:", payload);
-  
-    try {
-      const response = await fetch('https://informat.sa.ngrok.io/tesoreria/api/bancos/api_crear_banco/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
-  
-      const data = await response.json();
-      console.log("Response data:", data);
-    } catch (error) {
-      console.error("Error al enviar los datos:", error);
-    }
-  };
-  
-
+ 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white w-11/12 md:w-4/5 lg:w-2/3 xl:w-1/2 p-6 rounded shadow-lg relative">
-        <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-          onClick={onClose}
-        >
-          &times;
-        </button>
-        <h2 className="text-xl font-semibold mb-4">Cuenta corriente</h2>
-        <p className="mb-4">
+      <div className="bg-white w-11/12 md:w-4/5 lg:w-2/3 xl:w-1/2 p-4 rounded shadow-lg relative">
+      <button
+        className="absolute top-2 right-4 text-gray-500 hover:text-gray-700 text-2xl"
+        onClick={onClose}
+      >
+        &times;
+      </button>
+        <h2 className="text-lg font-semibold mb-3">Cuenta corriente</h2>
+        <p className="mb-3 text-sm text-gray-700">
           Carga la/las cuentas corrientes para el banco seleccionado
         </p>
-        <div className="flex items-center mb-4">
+        <div className="flex items-center mb-3">
           <input
             type="text"
             placeholder="Search"
             value={searchTerm}
             onChange={handleSearchChange}
-            className="border rounded p-2 w-full"
+            className="border rounded p-2 w-full text-sm"
           />
         </div>
-        <table className="w-full table-auto border-collapse">
+        <table className="w-full table-auto border-collapse text-sm">
           <thead>
             <tr>
-              <th className="border p-2">
+              <th className="border p-2 text-left">
                 <input
                   type="checkbox"
                   className="mr-2"
@@ -250,11 +216,11 @@ const AccountsModal = ({ isOpen, onClose, data, onLoad }) => {
                 />
                 Banco
               </th>
-              <th className="border p-2">Número de cuenta</th>
-              <th className="border p-2">Moneda</th>
-              <th className="border p-2">Saldo Actual</th>
-              <th className="border p-2">Saldo disponible</th>
-              <th className="border p-2">Cuentas Contables</th>
+              <th className="border p-2 text-left">Número de cuenta</th>
+              <th className="border p-2 text-left">Moneda</th>
+              <th className="border p-2 text-left">Saldo Actual</th>
+              <th className="border p-2 text-left">Saldo disponible</th>
+              <th className="border p-2 text-left">Cuentas Contables</th>
             </tr>
           </thead>
           <tbody>
@@ -265,10 +231,10 @@ const AccountsModal = ({ isOpen, onClose, data, onLoad }) => {
                   label: `${cuenta.numero_cuenta} - ${cuenta.nombre_cuenta}`,
                   disabled: Object.values(selectedOption).includes(cuenta.numero_cuenta)
                 }));
-
+  
                 return (
                   <tr key={account.id}>
-                    <td className="border p-2 text-center">
+                    <td className="border p-2 text-left">
                       <input
                         type="checkbox"
                         className="mr-2"
@@ -293,66 +259,63 @@ const AccountsModal = ({ isOpen, onClose, data, onLoad }) => {
               })
             ) : (
               <tr>
-                <td colSpan="6" className="border p-2 text-center">
+                <td colSpan="6" className="border p-2 text-center text-sm text-gray-600">
                   No hay datos disponibles
                 </td>
               </tr>
             )}
           </tbody>
         </table>
-        <div className="flex justify-between items-center mt-4">
-          <div>
-            <span>Mostrar </span>
-            <select
-              value={rowsPerPage}
-              onChange={handleRowsPerPageChange}
-              className="border p-2"
-            >
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-              <option value={15}>15</option>
-            </select>
-          </div>
-        </div>
-        <div className="flex justify-between items-center mt-4">
-          <button
-            className="bg-gray-200 p-2 rounded mr-2"
-            disabled={currentPage === 1}
-            onClick={() => handlePageChange(currentPage - 1)}
-          >
-            Anterior
-          </button>
-          <span>
-            Página {currentPage} de {totalPages}
-          </span>
-          <button
-            className="bg-gray-200 p-2 rounded ml-2"
-            disabled={currentPage === totalPages}
-            onClick={() => handlePageChange(currentPage + 1)}
-          >
-            Siguiente
-          </button>
-        </div>
-        <div className="flex justify-end mt-4">
-          <button className="bg-gray-200 p-2 rounded mr-2" onClick={onClose}>
-            Cambiar banco
-          </button>
-          <button
-            className="bg-red-500 text-white p-2 rounded"
-            onClick={handleLoadClick}
-          >
-            Cargar
-          </button>
-          <button
-            className="bg-green-500 text-white p-2 rounded"
-            onClick={handleSubmit}
-          >
-            Enviar
-          </button>
-        </div>
+        <div className="flex justify-end items-center mt-4">
+  <div className="text-sm mr-4">
+    <span>Filas por página: </span>
+    <select
+      value={rowsPerPage}
+      onChange={handleRowsPerPageChange}
+      className="p-1 text-sm focus:ring-0 focus:border-transparent border-0"
+    >
+      <option value={5}>5</option>
+      <option value={10}>10</option>
+      <option value={15}>15</option>
+    </select>
+  </div>
+  <div className="flex items-center text-sm">
+    <span className="mr-2">
+      {1}-{rowsPerPage} de {totalPages}
+    </span>
+    <button
+      className="p-1.5 rounded text-sm mr-2 hover:bg-gray-200 focus:outline-none"
+      disabled={currentPage === 1}
+      onClick={() => handlePageChange(currentPage - 1)}
+    >
+      &lt;
+    </button>
+    <button
+      className="p-1.5 rounded text-sm hover:bg-gray-200 focus:outline-none"
+      disabled={currentPage === totalPages}
+      onClick={() => handlePageChange(currentPage + 1)}
+    >
+      &gt;
+    </button>
+  </div>
+</div>
+
+<div className="flex justify-end mt-4">
+  <button className="border border-red-500 text-red-500 p-1.5 rounded mr-2 text-sm" onClick={onClose}>
+    Cambiar banco
+  </button>
+  <button
+    className="bg-red-500 text-white p-1.5 rounded text-sm"
+    onClick={handleLoadClick}
+  >
+    Cargar
+  </button>          
+</div>
+
       </div>
     </div>
   );
+  
 };
 
 export default AccountsModal;
